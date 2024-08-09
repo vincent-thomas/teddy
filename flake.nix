3415d8e2680a7d459e394aa9d9d4a5bf9d8a7a86
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2024-08-09 02:26:19 +0200

    testing gh actions

--- flake.nix
+++ flake.nix
@@ -10,34 +10,27 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    rust-overlay,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [
-            (import rust-overlay)
-          ];
+          overlays = [ (import rust-overlay) ];
         };
       in {
         devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             (rust-bin.stable.latest.default.override {
-                  extensions = [ "rust-src" "cargo" "rustc" ];
+              extensions = [ "rust-src" "cargo" "rustc" ];
             })
             gcc
           ];
 
-          RUST_SRC_PATH = "${pkgs.rust-bin.stable.latest.default.override {
-              extensions = [ "rust-src" ];
-          }}/lib/rustlib/src/rust/library";
-
+          RUST_SRC_PATH = "${
+              pkgs.rust-bin.stable.latest.default.override {
+                extensions = [ "rust-src" ];
+              }
+            }/lib/rustlib/src/rust/library";
 
           buildInputs = with pkgs; [
             openssl.dev
@@ -47,10 +40,10 @@
             clippy
             rust-analyzer
             cargo-watch
+            cargo-nextest
             bacon
           ];
         };
-      }
-    );
+      });
 }
 
